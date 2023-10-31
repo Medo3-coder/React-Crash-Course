c8f8ff3e80220a6e21767d5cdeeed940791589f5
Mohamed Farouk <59924879+Medo3-coder@users.noreply.github.com>
2023-10-31 11:56:41 +0200

    defaultProps

--- test/src/App.js
+++ test/src/App.js
@@ -2,21 +2,48 @@
 import './App.css';
 import React from 'react'
 
+// ------------------- defaultProps-------------
+
 function App() {
 
   return (
    <>
    <Hello name="conner"/>
    <Hello name="celement"/>
+   <Hello />
 
    </>
   );
 }
 
-function Hello(props)
+
+function Hello({name})
 {
-  return <h1>Hello {props.name}</h1>
+  return <h1>Hello {name}</h1>
 }
+
+Hello.defaultProps = {
+  name:"User"
+}
+
+
+// -------------------props-------------
+
+// function App() {
+
+//   return (
+//    <>
+//    <Hello name="conner"/>
+//    <Hello name="celement"/>
+
+//    </>
+//   );
+// }
+
+// function Hello(props)
+// {
+//   return <h1>Hello {props.name}</h1>
+// }
 
 
 export default App;
