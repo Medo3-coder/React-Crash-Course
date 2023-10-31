6a4898905ba466f527318f8503028ac33e232980
Mohamed Farouk <59924879+Medo3-coder@users.noreply.github.com>
2023-11-01 00:28:21 +0200

    useState and reducer

--- test/src/App.js
+++ test/src/App.js
@@ -1,41 +1,80 @@
 // import logo from './logo.svg';
 import './App.css';
-import React from 'react'
+import React, { useReducer, useState } from 'react'
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + action.num };
+    case 'decrement':
+      return { count: state.count + action.num };
+    default:
+      throw new Error('Unknown action type');
+  }
+}
+
+
+function App() {
+
+  const [state , dispatch] = useReducer(reducer , {
+    count: 0
+  });
+
+  return (
+    <>
+      <Counter count={state.count} onClick={()=>dispatch({
+        type: 'increment',
+        num:1  //inital number
+      })}/>
+    <Counter count={state.count} onClick={()=>dispatch({
+        type: 'increment',
+        num:1  //inital number
+      })}/>    </>
+  );
+}
+
+function Counter({ count, onClick }) {
+  return (
+    <>
+      <button onClick={onClick}>
+        Increment
+      </button>
+      <p>Count: {count}</p>
+    </>
+  );
+}
+
+
 
 
 // -----------Event-Driven Programming--------------------
 
 
+// function App() {
 
+//   return (
+//     <MyButton onClick={handleClick}>Click Me</MyButton>
+//   );
 
+//   function handleClick (event) {
+//     console.log(event.nativeEvent)
+//   };
 
+//   function MyButton(props)
+//   {
+//     return (
+//       <button
+//       onClick={props.onClick}   //{...props}  we can use spread operator
+//       // {...props} 
+//       style={{
+//         color:'red'
+//       }}>
+//         {props.children}
+//       </button>
+//     );
+//   }
 
-
-function App() {
-
-  return (
-    <MyButton onClick={handleClick}>Click Me</MyButton>
-  );
-
-  function handleClick (event) {
-    console.log(event.nativeEvent)
-  };
-
-  function MyButton(props)
-  {
-    return (
-      <button
-      onClick={props.onClick}   //{...props}  we can use spread operator
-      // {...props} 
-      style={{
-        color:'red'
-      }}>
-        {props.children}
-      </button>
-    );
-  }
-
-}
+// }
 
 
 
