aaac8f26b6c67329faa564272734bcb6b1cb01cf
Mohamed Farouk <59924879+Medo3-coder@users.noreply.github.com>
2023-10-31 13:28:17 +0200

    end props

--- test/src/App.js
+++ test/src/App.js
@@ -2,29 +2,54 @@
 import './App.css';
 import React from 'react'
 
-// ------------------- defaultProps-------------
+
+//-----------comment props
 
 function App() {
 
   return (
-   <>
-   <Hello name="conner"/>
-   <Hello name="celement"/>
-   <Hello />
-
-   </>
+     <Comment username="conner" time={(new Date()).toString()}>
+      <h1>Hello World</h1>
+     </Comment>
   );
 }
 
 
-function Hello({name})
+function Comment({username , time , children})
 {
-  return <h1>Hello {name}</h1>
+  return (
+    <section>
+      <p>{username} commented At {time}</p>
+      {children}
+    </section>
+  );
 }
 
-Hello.defaultProps = {
-  name:"User"
-}
+
+
+// ------------------- defaultProps-------------
+
+// function App() {
+
+//   return (
+//    <>
+//    <Hello name="conner"/>
+//    <Hello name="celement"/>
+//    <Hello />
+
+//    </>
+//   );
+// }
+
+
+// function Hello({name})
+// {
+//   return <h1>Hello {name}</h1>
+// }
+
+// Hello.defaultProps = {
+//   name:"User"
+// }
 
 
 // -------------------props-------------
